67b4aab640727d640ceba798c416eb425ba36cbc
t4h4 <tahayasin@outlook.com>
2021-04-07 22:53:48 +0300

    Toolbox component'lar eklendi.

--- TaskFormApp/Form1.Designer.cs
+++ TaskFormApp/Form1.Designer.cs
@@ -29,13 +29,67 @@ namespace TaskFormApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnReadFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnReadFile
+            // 
+            this.BtnReadFile.Location = new System.Drawing.Point(56, 31);
+            this.BtnReadFile.Name = "BtnReadFile";
+            this.BtnReadFile.Size = new System.Drawing.Size(94, 29);
+            this.BtnReadFile.TabIndex = 0;
+            this.BtnReadFile.Text = "Dosya Oku";
+            this.BtnReadFile.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(56, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(245, 244);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnCounter
+            // 
+            this.BtnCounter.Location = new System.Drawing.Point(460, 31);
+            this.BtnCounter.Name = "BtnCounter";
+            this.BtnCounter.Size = new System.Drawing.Size(94, 29);
+            this.BtnCounter.TabIndex = 2;
+            this.BtnCounter.Text = "Sayaç Arttır";
+            this.BtnCounter.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(460, 87);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(125, 27);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.BtnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnReadFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnReadFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
     }
 }
 
