c51901d7c15883b379d20c2a3c72d44654cb1d67
t4h4 <tahayasin@outlook.com>
2021-04-11 19:30:41 +0300

    ContinueWith(calis) method eklendi.

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -6,16 +6,21 @@ namespace TaskConsoleApp
 {
     class Program
     {
+        public static void calis(Task<string> data)
+        {
+            // 100 satirlik kod diyelim. ayrı method olusturularak okunabilirlik daha da artti.
+            Console.WriteLine("data uzunluk: " + data.Result.Length); // task oldugu icin resul ile sonuc aldik mecburen. 
+        }
         private async static Task Main(string[] args) // donus tipi asenkron oldugundan void degil task. 
         {
             Console.WriteLine("Hello World!");
 
-            var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+            var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(calis);
                
             Console.WriteLine("arada yapilacak isler");
 
-            var data = await mytask;
-            Console.WriteLine("data uzunluk: " + data.Length);
+            await mytask;
+            
         }
     }
 }
