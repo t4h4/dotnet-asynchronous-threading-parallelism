74f84c1987e99a48f7d0e1170190ef1de0131098
t4h4 <tahayasin@outlook.com>
2021-04-11 19:09:05 +0300

    Console projesi oluşturuldu.

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
