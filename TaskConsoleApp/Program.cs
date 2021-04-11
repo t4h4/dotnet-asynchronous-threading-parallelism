8a3c19c3623421a2b2cae30d9606a04ad6c7db1c
t4h4 <tahayasin@outlook.com>
2021-04-11 19:21:25 +0300

    ContinueWith method eklendi.

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private async static Task Main(string[] args) // donus tipi asenkron oldugundan void degil task. 
         {
             Console.WriteLine("Hello World!");
+
+            var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith
+                ((data) => // data google'dan gelen veri. 
+            {
+                Console.WriteLine("data uzunluk: " + data.Result.Length);
+            });
+
+            Console.WriteLine("arada yapilacak isler");
+
+            await mytask;
         }
     }
 }
