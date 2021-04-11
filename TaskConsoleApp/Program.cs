78f5c0232aebd139daef2be322c5f4a2def979d0
t4h4 <tahayasin@outlook.com>
2021-04-11 21:45:47 +0300

    Task Method: WhenAll(task1,task2,task3,task4,….) eklendi.

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,26 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
-    class Program
+    public class Content
     {
-        public static void calis(Task<string> data)
+        public string Site { get; set; }
+        public int Len { get; set; }
+    }
+
+    internal class Program
+    {
+        private async static Task Main(string[] args)
         {
-            // 100 satirlik kod diyelim. ayrı method olusturularak okunabilirlik daha da artti.
-            Console.WriteLine("data uzunluk: " + data.Result.Length); // task oldugu icin resul ile sonuc aldik mecburen. 
+            Console.WriteLine("Main thread: " + Thread.CurrentThread.ManagedThreadId);
+            List<string> urlsList = new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com",
+                "https://www.facebook.com",
+                "https://www.oracle.com",
+                "https://www.t4h4.net"
+            };
+
+            List<Task<Content>> taskList = new List<Task<Content>>();
+
+            urlsList.ToList().ForEach(x =>
+            {
+                taskList.Add(GetContentAsync(x));
+            });
+
+            var contents = Task.WhenAll(taskList.ToArray()); // WhenAll methodu parametre olarak task array alir, array icindeki task'lerin hepsi tamamlayincaya kadar bekler. 
+
+            Console.WriteLine("WhenAll method sonrasi baska isler yapildi.");
+
+            var data = await contents;
+
+            data.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Site} boyut:{x.Len}");
+            });
         }
-        private async static Task Main(string[] args) // donus tipi asenkron oldugundan void degil task. 
+
+        public static async Task<Content> GetContentAsync(string url) // asenkron methodlar geriye task doner. donenin tipi Content.
         {
-            Console.WriteLine("Hello World!");
+            Content c = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
 
-            var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(calis);
-               
-            Console.WriteLine("arada yapilacak isler");
+            c.Site = url;
+            c.Len = data.Length;
 
-            await mytask;
-            
+            Console.WriteLine("GetContentAsync thread: " + Thread.CurrentThread.ManagedThreadId); // o an calisan thread id'si yazdiriliyor.
+
+            return c;
         }
     }
 }
