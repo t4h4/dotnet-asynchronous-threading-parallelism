d29988a14ca996cf0667bdd061ada891669d85c1
t4h4 <tahayasin@outlook.com>
2021-04-13 17:26:19 +0300

    Senkron ProgressBar eklendi.

--- TaskThreadApp/Form1.cs
+++ TaskThreadApp/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,8 @@ namespace TaskThreadApp
 {
     public partial class Form1 : Form
     {
+        public static int Counter { get; set; } = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,22 @@ namespace TaskThreadApp
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            Go(progressBar1);
+            Go(progressBar2);
+        }
 
+        public void Go(ProgressBar pb)
+        {
+            Enumerable.Range(1, 100).ToList().ForEach(x =>
+            {
+                Thread.Sleep(10);
+                pb.Value = x;
+            });
         }
 
         private void btnCounter_Click(object sender, EventArgs e)
         {
-
+            btnCounter.Text = Counter++.ToString();
         }
     }
 }
