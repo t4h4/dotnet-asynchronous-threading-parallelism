0ee1b9b0ab6fe73fa89aa1b3fb4138409527cb66
t4h4 <tahayasin@outlook.com>
2021-04-13 17:41:51 +0300

    Task.Run() method eklendi. task run ile calistirmak istegimiz kodları ayri thread icerisinde calistiririz.

--- TaskThreadApp/Form1.cs
+++ TaskThreadApp/Form1.cs
@@ -20,19 +20,24 @@ namespace TaskThreadApp
             InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private async void btnStart_Click(object sender, EventArgs e)
         {
-            Go(progressBar1);
-            Go(progressBar2);
+            var aTask = Go(progressBar1);
+            var bTask = Go(progressBar2);
+
+            await Task.WhenAll(aTask, bTask);
         }
 
-        public void Go(ProgressBar pb)
+        public async Task Go(ProgressBar pb)
         {
-            Enumerable.Range(1, 100).ToList().ForEach(x =>
-            {
-                Thread.Sleep(10);
-                pb.Value = x;
-            });
+            await Task.Run(() => { // task run ile calistirmak istegimiz kodları ayri thread icerisinde calistiririz. 
+                Enumerable.Range(1, 100).ToList().ForEach(x =>
+                {
+                    Thread.Sleep(10);
+                    // pb.Value = x; // farkli thread'den ui threade etki eden value degerine erisilemez. o yuzden asagidaki kullanim uygundur.
+                    pb.Invoke((MethodInvoker)delegate { pb.Value = x; });
+                });
+            });           
         }
 
         private void btnCounter_Click(object sender, EventArgs e)
