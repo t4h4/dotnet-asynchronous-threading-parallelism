f70499c752775f07587d0b1b7af5a7c7366a2fdb
t4h4 <tahayasin@outlook.com>
2021-04-13 18:45:49 +0300

    Cancellation token eklendi.

--- TaskCancellationFormApp/Form1.cs
+++ TaskCancellationFormApp/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,19 +14,40 @@ namespace TaskCancellationFormApp
 {
     public partial class Form1 : Form
     {
+        CancellationTokenSource ct = new CancellationTokenSource();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
+
+            try
+            {
+                Task<HttpResponseMessage> mytask;
+
+                mytask = new HttpClient().GetAsync("https://www.baidu.com/", ct.Token);
+
+                await mytask;
+
+                var content = await mytask.Result.Content.ReadAsStringAsync();
+
+                richTextBox1.Text = content;
+            }
+            catch (TaskCanceledException ex)
+            {
+                //TaskCanceledException
+
+                MessageBox.Show(ex.Message);
+            }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            ct.Cancel();
         }
     }
 }
