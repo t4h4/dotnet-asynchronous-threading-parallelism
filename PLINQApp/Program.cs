c192aa2272957de9816c1214a91f3df2679ccbab
t4h4 <tahayasin@outlook.com>
2021-04-15 00:18:16 +0300

    AsParallel() method eklendi.

---
+++ PLINQApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PLINQApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch(); // kronometre
+            sw.Start(); // kronometre basladi.
+            var array = Enumerable.Range(1, 50000).ToList();
+
+            var newArray = array.AsParallel().Where(x => x % 2 == 0);
+
+            newArray.ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+            sw.Stop(); // kronometre bitti. 
+            Console.WriteLine("işlem bitti. bu kadar milisaniye sürdü: " + sw.ElapsedMilliseconds);
+        }
+    }
+}
