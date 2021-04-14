ca240d171941b6fcaef2cb823022c78b8c976762
t4h4 <tahayasin@outlook.com>
2021-04-14 22:59:00 +0300

    Thread-Local Variables eklendi.

---
+++ ThreadLocalVariablesApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadLocalVariablesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            // subtotal o anki thread'in kendi icerisinde hesaplayacagi deger. 
+            // () => 0 lokal baslangic degeri 0
+            // loop donguyu temsil ediyor. 
+            Parallel.ForEach(Enumerable.Range(1, 100).ToList(), () => 0, (x, loop, subtotal) =>
+              {
+                  subtotal += x;
+                  return subtotal;
+              }, (y) => Interlocked.Add(ref total, y));
+
+            Console.WriteLine(total);
+        }
+    }
+}
