7b54647531f499b59923fbdd014684cbbeaf4a9d
t4h4 <tahayasin@outlook.com>
2021-04-14 18:49:48 +0300

    Interlocked ile race condition engellendi.

--- InterlockedParallelForEachApp/Program.cs
+++ InterlockedParallelForEachApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace InterlockedParallelForEachApp
 {
@@ -6,7 +9,23 @@ namespace InterlockedParallelForEachApp
     {
         static void Main(string[] args)
         {
-            
+            // dosyalarin boyutunu alacagiz ve ekleye ekleye asagidaki degiskene atayacagiz. 
+
+            long FilesByte = 0;
+
+            string picturesPath = @"C:\Users\tahay\Desktop\Asynchronous-Threading\ForEachParallelApp\pictures\";
+            var files = Directory.GetFiles(picturesPath);
+
+            Parallel.ForEach(files, (item) => {
+                Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+
+                FileInfo f = new FileInfo(item);
+
+                Interlocked.Add(ref FilesByte, f.Length); // race condition engelliyor, kilitliyor. 
+                
+            });
+
+            Console.WriteLine("Dosyalarin toplam boyutu: " + FilesByte);
         }
     }
 }
