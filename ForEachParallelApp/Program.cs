8ea7c05ec06ade5e58675195cf34d95e83c34089
t4h4 <tahayasin@outlook.com>
2021-04-14 18:35:36 +0300

    Parallel.ForEach eklendi.

--- ForEachParallelApp/Program.cs
+++ ForEachParallelApp/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ForEachParallelApp
 {
@@ -6,7 +11,23 @@ namespace ForEachParallelApp
     {
         static void Main(string[] args)
         {
-            
+            Stopwatch sw = new Stopwatch(); // kronometre
+            sw.Start(); // kronometre basladi.
+
+            string picturesPath = @"C:\Users\tahay\Desktop\Asynchronous-Threading\ForEachParallelApp\pictures\";
+            var files = Directory.GetFiles(picturesPath);
+
+            Parallel.ForEach(files, (item) => {
+            Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+            Image img = new Bitmap(item);
+
+            var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+            thumbnail.Save(Path.Combine(picturesPath, "thumbnail", Path.GetFileName(item)));
+            });
+
+            sw.Stop(); // kronometre bitti. 
+            Console.WriteLine("işlem bitti. bu kadar milisaniye sürdü: " + sw.ElapsedMilliseconds);
         }
     }
 }
