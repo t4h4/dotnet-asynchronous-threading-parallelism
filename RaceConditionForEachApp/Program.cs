6efb87e5c43402f6a330cc391fbf8e9f4cd29591
t4h4 <tahayasin@outlook.com>
2021-04-14 19:05:44 +0300

    Race condition sağlandı.

---
+++ RaceConditionForEachApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaceConditionForEachApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger = 0;
+
+            Parallel.ForEach(Enumerable.Range(1, 10000000).ToList(), (x) =>
+            {
+                deger = x;
+            });
+
+            Console.WriteLine(deger);
+        }
+    }
+}
