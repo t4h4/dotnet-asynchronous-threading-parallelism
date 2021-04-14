bc7ade214b5f230744188239705aefff24d22742
t4h4 <tahayasin@outlook.com>
2021-04-14 19:18:12 +0300

    Parallel.For eklendi.

---
+++ ParallelForApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long totalByte = 0;
+            var files = Directory.GetFiles(@"C:\Users\tahay\Desktop\Asynchronous-Threading\ForEachParallelApp\pictures\");
+
+            Parallel.For(0, files.Length, (index) =>
+            {
+                var file = new FileInfo(files[index]);
+                Interlocked.Add(ref totalByte, file.Length);
+            });
+
+            Console.WriteLine(totalByte);
+        }
+    }
+}
