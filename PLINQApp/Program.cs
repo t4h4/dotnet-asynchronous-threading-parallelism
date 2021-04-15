cd6a805209766c9b30018b7fb063257a47730920
t4h4 <tahayasin@outlook.com>
2021-04-15 04:25:47 +0300

    Basic select sorgusu yazıldı.

--- PLINQApp/Program.cs
+++ PLINQApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PLINQApp.Models;
+using System;
 using System.Diagnostics;
 using System.Linq;
 
@@ -8,7 +9,11 @@ namespace PLINQApp
     {
         static void Main(string[] args)
         {
-            
+            AdventureWorks2017Context context = new AdventureWorks2017Context();
+            context.Products.Take(10).ToList().ForEach(x => // 10 tanesini al
+           {
+               Console.WriteLine(x.Name);
+           });
         }
     }
 }
