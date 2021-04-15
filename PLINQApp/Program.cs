ac8bda01d2a075ed35649a8575f3f05e0d23844a
t4h4 <tahayasin@outlook.com>
2021-04-15 17:33:53 +0300

    WithExecutionMode() eklendi.

--- PLINQApp/Program.cs
+++ PLINQApp/Program.cs
@@ -11,7 +11,7 @@ namespace PLINQApp
         {
             AdventureWorks2017Context context = new AdventureWorks2017Context();
 
-            var product = (from p in context.Products.AsParallel().WithDegreeOfParallelism(2) // 2 thread'de calisacak.
+            var product = (from p in context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism) // zorla parallel. normalde parallel calisip calismayacagina program karar veriyor. 
                            where p.ListPrice > 10M
                            select p).Take(10);
             product.ForAll(x =>
