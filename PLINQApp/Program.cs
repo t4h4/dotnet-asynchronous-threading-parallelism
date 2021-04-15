558d00348875cf6bb459c8c72ebd67280268da30
t4h4 <tahayasin@outlook.com>
2021-04-15 04:08:06 +0300

    Reset

--- PLINQApp/Program.cs
+++ PLINQApp/Program.cs
@@ -8,18 +8,7 @@ namespace PLINQApp
     {
         static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch(); // kronometre
-            sw.Start(); // kronometre basladi.
-            var array = Enumerable.Range(1, 50000).ToList();
-
-            var newArray = array.AsParallel().Where(x => x % 2 == 0);
-
-            newArray.ToList().ForEach(x =>
-            {
-                Console.WriteLine(x);
-            });
-            sw.Stop(); // kronometre bitti. 
-            Console.WriteLine("işlem bitti. bu kadar milisaniye sürdü: " + sw.ElapsedMilliseconds);
+            
         }
     }
 }
